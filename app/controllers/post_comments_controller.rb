04f0a818cfd2a6bbd45cecce41bb8745f5b5b758
someya.kotaro.infratop@gmail.com <ec2-user@ip-172-31-10-218.ec2.internal>
2021-09-25 02:13:43 +0000

    [Start] 4-4

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,7 @@
+class PostCommentsController < ApplicationController
+  def created
+  end
+  
+  def destroy
+  end
+end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,4 +2,5 @@ class PostImage < ApplicationRecord
   
   belongs_to :user
   attachment :image
+  has_many :post_comment, dependent: :destroy
 end
